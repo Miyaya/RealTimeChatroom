f4a069727d541d5434c99fa2c9f8df572431d11c
Miya <miya850604@gmail.com>
2021-08-11 17:38:44 +0200

    add logo

--- RealTimeChatApp/Controllers/Login/LoginViewController.swift
+++ RealTimeChatApp/Controllers/Login/LoginViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +25,17 @@ class LoginViewController: UIViewController {
                                                             style: .done,
                                                             target: self,
                                                             action: #selector(didTapRegister))
+        
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let size = view.width/3
+        imageView.frame = CGRect(x: (view.width-size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
     }
     
     // push the register controller to screen
